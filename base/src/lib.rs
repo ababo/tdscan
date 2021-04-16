8587ef690691787d06e61450eae3e7be1bb69048
Simon Prykhodko <semion.ababo@gmail.com>
2021-04-16 19:48:52 +0200

    Create 'base' crate with an initial protobuf model

---
+++ base/src/lib.rs
@@ -0,0 +1,11 @@
+pub mod model {
+    include!(concat!(env!("OUT_DIR"), "/base.model.rs"));
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}

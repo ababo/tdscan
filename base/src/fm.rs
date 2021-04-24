2da777f26aa7341c401e6c3c709e19b861e9f6b6
Simon Prykhodko <semion.ababo@gmail.com>
2021-04-24 20:33:00 +0200

    Replace Brotli compression with GZIP
    This change reduces size of wasm-file from 1.3MiB to 100KiB.

--- base/src/fm.rs
+++ base/src/fm.rs
@@ -1,8 +1,8 @@
-use std::io::{Read, Write};
+use std::io::{copy, Read, Write};
 use std::str::FromStr;
 use structopt::StructOpt;
 
-use brotli;
+use flate2;
 use prost::Message;
 
 use crate::defs::{Error, ErrorKind::*, IntoResult, Result};
@@ -11,13 +11,13 @@ use crate::model::Model;
 const MAGIC: u32 = 0x01C3ADF8;
 const VERSION: u32 = 1;
 
-const DEFAULT_COMPRESSION: &'static str = "brotli";
-const DEFAULT_BROTLI_QUALITY: &'static str = "6";
+const DEFAULT_COMPRESSION: &'static str = "gzip";
+const DEFAULT_GZIP_LEVEL: &'static str = "6";
 
 #[derive(Clone, Copy)]
 pub enum Compression {
     None = 0,
-    Brotli = 1,
+    Gzip = 1,
 }
 
 impl FromStr for Compression {
@@ -26,10 +26,20 @@ impl FromStr for Compression {
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
         match s {
             "none" => Ok(Compression::None),
-            "brotli" => Ok(Compression::Brotli),
-            _ => Err("unsupported compression"),
+            "gzip" => Ok(Compression::Gzip),
+            _ => Err("can be 'none' or 'gzip'"),
         }
     }
+}
+
+fn validate_gzip_level(value: String) -> std::result::Result<(), String> {
+    let parsed = value
+        .parse::<u32>()
+        .map_err(|_| "failed to parse GZIP compression level".to_string())?;
+    if parsed > 9 {
+        return Err("can be from 0 to 9".to_string());
+    }
+    Ok(())
 }
 
 #[derive(StructOpt)]
@@ -41,18 +51,19 @@ pub struct Params {
     )]
     pub compression: Compression,
     #[structopt(
-        name = "fm-brotli-quality",
-        default_value = DEFAULT_BROTLI_QUALITY,
-        long
+        name = "fm-gzip-level",
+        default_value = DEFAULT_GZIP_LEVEL,
+        long,
+        validator = validate_gzip_level
     )]
-    pub brotli_quality: u32,
+    pub gzip_level: u32,
 }
 
 impl Default for Params {
     fn default() -> Self {
         Self {
             compression: Compression::from_str(DEFAULT_COMPRESSION).unwrap(),
-            brotli_quality: DEFAULT_BROTLI_QUALITY.parse::<u32>().unwrap(),
+            gzip_level: DEFAULT_GZIP_LEVEL.parse::<u32>().unwrap(),
         }
     }
 }
@@ -62,37 +73,34 @@ pub fn encode<W: Write>(
     params: &Params,
     mut writer: W,
 ) -> Result<()> {
-    let _ = writer
+    writer
         .write(&MAGIC.to_le_bytes())
         .res("failed to write .fm magic".to_string())?;
-    let _ = writer
+    writer
         .write(&VERSION.to_le_bytes())
         .res("failed to write .fm version".to_string())?;
-    let _ = writer
+    writer
         .write(&(params.compression as i32).to_le_bytes())
         .res("failed to write .fm compression".to_string())?;
+
+    let mut enc_writer: Box<dyn Write>;
+
+    match params.compression {
+        Compression::None => {
+            enc_writer = Box::new(writer);
+        }
+        Compression::Gzip => {
+            let compression = flate2::Compression::new(params.gzip_level);
+            let encoder = flate2::write::GzEncoder::new(writer, compression);
+            enc_writer = Box::new(encoder);
+        }
+    }
 
     let mut buf = Vec::with_capacity(model.encoded_len());
     model.encode(&mut buf).unwrap();
 
-    match params.compression {
-        Compression::None => {
-            let _ = writer
-                .write(&buf)
-                .res("failed to write .fm model".to_string())?;
-        }
-        Compression::Brotli => {
-            let mut writer = brotli::CompressorWriter::new(
-                writer,
-                0,
-                params.brotli_quality,
-                0,
-            );
-            let _ = writer.write(&buf).res(
-                "failed to write Brotli-compressed .fm model".to_string(),
-            )?;
-        }
-    }
+    copy(&mut buf.as_slice(), &mut enc_writer)
+        .res("failed to write .fm model".to_string())?;
 
     Ok(())
 }
@@ -113,7 +121,7 @@ pub fn decode<R: Read>(mut reader: R) -> Result<Model> {
 
     reader
         .read(&mut buf)
-        .res("failed to read .fm magic".to_string())?;
+        .res("failed to read .fm version".to_string())?;
     let val = u32::from_le_bytes(buf);
     if val != VERSION {
         return Err(Error::new(
@@ -124,27 +132,32 @@ pub fn decode<R: Read>(mut reader: R) -> Result<Model> {
 
     reader
         .read(&mut buf)
-        .res("failed to read .fm magic".to_string())?;
+        .res("failed to read .fm compression".to_string())?;
     let val = i32::from_le_bytes(buf);
 
     const COMPRESSION_NONE: i32 = Compression::None as i32;
-    const COMPRESSION_BROTLI: i32 = Compression::Brotli as i32;
-    let mut buf = vec![];
+    const COMPRESSION_GZIP: i32 = Compression::Gzip as i32;
+
+    let mut dec_reader: Box<dyn Read>;
 
     match val {
         COMPRESSION_NONE => {
-            reader
-                .read_to_end(&mut buf)
-                .res("failed to read .fm model".to_string())?;
+            dec_reader = Box::new(reader);
         }
-        COMPRESSION_BROTLI => {}
+        COMPRESSION_GZIP => {
+            dec_reader = Box::new(flate2::read::GzDecoder::new(reader));
+        }
         _ => {
-            let mut reader = brotli::Decompressor::new(reader, 0);
-            reader.read_to_end(&mut buf).res(
-                "failed to read Brotli-compressed .fm model".to_string(),
-            )?;
+            return Err(Error::new(
+                MalformedData,
+                format!("unknown compression '{}'", val),
+            ));
         }
     }
+
+    let mut buf = vec![];
+    copy(&mut dec_reader, &mut buf)
+        .res("failed to read .fm model".to_string())?;
 
     let model = Model::decode(buf.as_slice())
         .res("failed to decode .fm model".to_string())?;

74d6442f6034b7496adc9c013748454571ce65aa
Simon Prykhodko <semion.ababo@gmail.com>
2021-07-03 19:05:23 +0200

    Add a newly created scanner iOS project

---
+++ scanner/ios/Fitsme Scanner/ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerApp.swift
+//  Fitsme Scanner
+//
+//  Created by Simon Prykhodko on 03/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

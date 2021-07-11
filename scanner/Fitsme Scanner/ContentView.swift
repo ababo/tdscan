04154ec98cba5037300eab088fb3f74c99881890
Simon Prykhodko <semion.ababo@gmail.com>
2021-07-11 11:00:58 +0200

    Start web server on port 8080

--- scanner/Fitsme Scanner/ContentView.swift
+++ scanner/Fitsme Scanner/ContentView.swift
@@ -1,17 +1,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Dimmer {
-            Text("Main View")
-        }
+  var body: some View {
+    Dimmer {
+      Text("Main View")
     }
+  }
 }
 
 #if DEBUG
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+  struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+      ContentView()
     }
+  }
 #endif
--- scanner/Fitsme Scanner/ScannerApp.swift
+++ scanner/Fitsme Scanner/ScannerApp.swift
@@ -2,11 +2,41 @@ import SwiftUI
 
 @main
 struct ScannerApp: App {
-    // @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+  let webServer = GCDWebServer()
 
-    var body: some Scene {
-        return WindowGroup {
-            ContentView()
-        }
+  init() {
+    webServer.addDefaultHandler(
+      forMethod: "GET",
+      request: GCDWebServerRequest.self,
+      processBlock: handleRequest)
+    startServer()
+  }
+
+  var body: some Scene {
+    return WindowGroup {
+      ContentView()
     }
+  }
+
+  func handleRequest(request: GCDWebServerRequest)
+    -> GCDWebServerResponse?
+  {
+    let resp = GCDWebServerStreamedResponse.init(
+      contentType: "text/plain",
+      asyncStreamBlock: { block in
+        block("this is a block ".data(using: .utf8), nil)
+      })
+    return resp
+  }
+
+  func startServer() {
+    let options: [String: Any] = [
+      "AutomaticallySuspendInBackground": false,
+      "BindToLocalhost": false,
+      "BonjourName": "Fitsme Server",
+      "ConnectedStateCoalescingInterval": 2.0,
+      "Port": 8080,
+    ]
+    try! webServer.start(options: options)
+  }
 }

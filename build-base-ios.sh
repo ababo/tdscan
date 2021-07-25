d5f22c0c47e5c0d7e88552adc595248b7c922712
Simon Prykhodko <semion.ababo@gmail.com>
2021-07-25 19:51:18 +0200

    Make `Fitsme Scanner` Xcode-project build and link with `base`

---
+++ build-base-ios.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# This script is mainly to be called by `Fitsme Scanner` Xcode-project that
+# links with `base` static library for `aarch64-apple-ios` target.
+
+if [ -z $(which cargo) ]; then
+    PATH="$HOME/.cargo/bin:$PATH"
+fi
+
+PROJECT_PATH=$(dirname "$0")
+
+cd $PROJECT_PATH/base
+cargo build --target aarch64-apple-ios --release
+exit $?
--- build-viewer.sh
+++ build-viewer.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 # This script is a workaround for the lack of workspaces support in wasm-pack.
 # See https://github.com/rustwasm/wasm-pack/issues/642 for details.
+
+if [ -z $(which cargo) ]; then
+    PATH="$HOME/.cargo/bin:$PATH"
+fi
 
 PROJECT_PATH=$(dirname "$0")
 
 cd $PROJECT_PATH/viewer
 wasm-pack build --target web "$@"
-STATUS=$?
-cd ..
-exit $STATUS
+exit $?

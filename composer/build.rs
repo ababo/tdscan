c0b95818867f80d83df842ceceb31506f217c487
Simon Prykhodko <semion.ababo@gmail.com>
2021-11-04 22:43:52 +0100

    Build PoissonRecon dependency

--- composer/build.rs
+++ composer/build.rs
@@ -5,10 +5,8 @@ use std::path::{Path, PathBuf};
 
 use zip::ZipArchive;
 
-fn target_dir<P: AsRef<Path>, I: IntoIterator<Item = P>>(iter: I) -> PathBuf {
-    let mut path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    path.extend(iter);
-    path
+fn target_dir() -> PathBuf {
+    PathBuf::from(env::var("OUT_DIR").unwrap())
 }
 
 fn unzip<P: AsRef<Path>, P2: AsRef<Path>>(zip: P, to: P2) {
@@ -50,26 +48,45 @@ fn unzip<P: AsRef<Path>, P2: AsRef<Path>>(zip: P, to: P2) {
 }
 
 fn build_poisson_recon() {
-    let lib = target_dir(["libPoissonRecon.a"]);
+    let lib = target_dir().join("libPoissonRecon.a");
     if lib.exists() {
         return;
     }
 
-    println!("Building libPoissonRecon.a");
+    const COMMIT: &str = "8683f6c44c2a3f03c10e456f1bdfae5fc69ec3f7";
 
-    let src = target_dir(["PoissonRecon"]);
-    if !src.exists() {
-        let zip = target_dir(["PoissonRecon.zip"]);
+    let name = format!("PoissonRecon-{}", COMMIT);
+    let proj = target_dir().join(&name);
+    if !proj.exists() {
+        let zip = target_dir().join("PoissonRecon.zip");
         if !zip.exists() {
             println!("Downloading PoissonRecon.zip");
             let mut file = File::create(&zip).unwrap();
-            const URL: &str = "https://github.com/mkazhdan/PoissonRecon/archive/refs/heads/master.zip";
-            let mut resp = ureq::get(URL).call().unwrap().into_reader();
+            let url = format!(
+                "https://github.com/CloudCompare/PoissonRecon/archive/{}.zip",
+                COMMIT
+            );
+            let mut resp = ureq::get(&url).call().unwrap().into_reader();
             copy(&mut resp, &mut file).unwrap();
         }
         println!("Unzipping PoissonRecon.zip");
-        unzip(zip, src);
+        unzip(zip, target_dir());
     }
+
+    println!("Building libPoissonRecon.a");
+    let src = proj.join("Src_CC_wrap");
+    cc::Build::new()
+        .cpp(true)
+        .include(proj.to_str().unwrap())
+        .flag_if_supported("-std=c++14")
+        .flag_if_supported("/std:c++14")
+        .opt_level(3)
+        .warnings(false)
+        .flag("-Wno-#pragma-messages")
+        .flag_if_supported("-Wno-cpp")
+        .file(src.join("PointData.cpp").to_str().unwrap())
+        .file(src.join("PoissonReconLib.cpp").to_str().unwrap())
+        .compile("libPoissonRecon.a");
 }
 
 fn main() {

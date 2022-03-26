052915d66b743a3de4710ce30e2a32f7e45987e7
Simon Prykhodko <semion.ababo@gmail.com>
2022-03-26 16:46:58 +0100

    Make build-view command output fm-file rather than obj

--- composer/src/build_view.rs
+++ composer/src/build_view.rs
@@ -1,7 +1,10 @@
+use std::io::Cursor;
+
+use image::ImageOutputFormat;
 use log::info;
 use structopt::StructOpt;
+use uuid::Uuid;
 
-use crate::export_obj::write_textured_mesh;
 use crate::mesh::Mesh;
 use crate::point_cloud::{build_frame_clouds, PointCloudParams, PointNormal};
 use crate::poisson;
@@ -9,6 +12,7 @@ use crate::scan::{read_scans, ScanParams};
 use crate::texture::{TextureParams, TexturedMesh};
 use base::defs::{Error, ErrorKind::*, Result};
 use base::fm;
+use base::fm::record::Type::*;
 use base::util::cli;
 
 #[derive(StructOpt)]
@@ -59,13 +63,24 @@ pub struct BuildViewParams {
     )]
     pub decimate_ratio: f64,
 
+    #[structopt(
+        help = "Disable texturing",
+        long,
+        short = "g",
+        conflicts_with = "texture", // TODO: Make it work somehow.
+    )]
+    pub disable_texturing: bool,
+
+    #[structopt(help = "Output element name", long, short = "e")]
+    pub element: Option<String>,
+
     #[structopt(flatten)]
     pub texture: TextureParams,
 }
 
 pub fn build_view(
     reader: &mut dyn fm::Read,
-    _writer: &mut dyn fm::Write,
+    writer: &mut dyn fm::Write,
     params: &BuildViewParams,
 ) -> Result<()> {
     info!("reading scans...");
@@ -119,15 +134,26 @@ pub fn build_view(
         mesh = mesh.decimate(params.decimate_ratio);
     }
 
-    info!(
-        "texturing mesh of {} vertices and {} faces...",
-        mesh.vertices.len(),
-        mesh.faces.len()
-    );
-    let tmesh = TexturedMesh::new(&scans, &scan_frames, mesh, &params.texture)?;
+    let (view, state) = if params.disable_texturing {
+        create_non_textured_element(params, &mesh)?
+    } else {
+        info!(
+            "texturing mesh of {} vertices and {} faces...",
+            mesh.vertices.len(),
+            mesh.faces.len()
+        );
+        let tmesh =
+            TexturedMesh::new(&scans, &scan_frames, mesh, &params.texture)?;
+        create_textured_element(params, &tmesh)?
+    };
 
-    info!("writing textured mesh...");
-    write_textured_mesh(&tmesh);
+    info!("writing generated model...");
+    writer.write_record(&fm::Record {
+        r#type: Some(ElementView(view)),
+    })?;
+    writer.write_record(&fm::Record {
+        r#type: Some(ElementViewState(state)),
+    })?;
 
     info!("done");
     Ok(())
@@ -151,4 +177,94 @@ impl poisson::Cloud<f64> for Cloud {
     fn normal(&self, index: usize) -> [f64; 3] {
         *self.0[index].1.as_ref()
     }
+}
+
+fn create_non_textured_element(
+    params: &BuildViewParams,
+    mesh: &Mesh,
+) -> Result<(fm::ElementView, fm::ElementViewState)> {
+    let element = params
+        .element
+        .clone()
+        .unwrap_or_else(|| Uuid::new_v4().to_string());
+
+    let mut view = fm::ElementView {
+        element,
+        ..Default::default()
+    };
+
+    view.faces = mesh
+        .faces
+        .iter()
+        .map(|f| fm::element_view::Face {
+            vertex1: f[0] as u32 + 1,
+            vertex2: f[1] as u32 + 1,
+            vertex3: f[2] as u32 + 1,
+            normal1: f[0] as u32 + 1,
+            normal2: f[1] as u32 + 1,
+            normal3: f[2] as u32 + 1,
+            ..Default::default()
+        })
+        .collect();
+
+    let mut state = fm::ElementViewState {
+        element: view.element.clone(),
+        ..Default::default()
+    };
+
+    state.vertices = mesh
+        .vertices
+        .iter()
+        .map(|p| fm::Point3 {
+            x: p[0] as f32,
+            y: p[1] as f32,
+            z: p[2] as f32,
+        })
+        .collect();
+
+    state.normals = mesh
+        .normals
+        .iter()
+        .map(|p| fm::Point3 {
+            x: p[0] as f32,
+            y: p[1] as f32,
+            z: p[2] as f32,
+        })
+        .collect();
+
+    Ok((view, state))
+}
+
+fn create_textured_element(
+    params: &BuildViewParams,
+    mesh: &TexturedMesh,
+) -> Result<(fm::ElementView, fm::ElementViewState)> {
+    let (mut view, state) = create_non_textured_element(params, &mesh.mesh)?;
+
+    let mut png = Cursor::new(Vec::new());
+    mesh.image
+        .write_to(&mut png, ImageOutputFormat::Png)
+        .unwrap();
+
+    view.texture = Some(fm::Image {
+        r#type: fm::image::Type::Png as i32,
+        data: png.into_inner(),
+    });
+
+    view.texture_points = mesh
+        .uv_coords
+        .iter()
+        .map(|p| fm::Point2 {
+            x: p.y as f32,
+            y: p.x as f32,
+        })
+        .collect();
+
+    for (i, idxs) in mesh.uv_idxs.iter().enumerate() {
+        view.faces[i].texture1 = idxs[0] as u32 + 1;
+        view.faces[i].texture2 = idxs[1] as u32 + 1;
+        view.faces[i].texture3 = idxs[2] as u32 + 1;
+    }
+
+    Ok((view, state))
 }
